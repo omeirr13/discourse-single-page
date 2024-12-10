192f2f2009a8783621c950289c4f6ff74cbe310d
AsharSal <asharsaleem4@gmail.com>
2024-12-10 18:56:35 +0500

    fix categroies didebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,11 @@
 import { useEffect, useState } from "react";
-import { sidebar_items } from "../constants";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCategories } from "../redux/features/categoriesSlice";
 
-const Item = ({ name, image, link }) => {
+const Item = ({ name }) => {
     return (
         <div className="flex gap-2">
-            <img src={`/images/sidebar/${image}`} />
-            <a href={link} target="_blank">
+            <a href="#" target="_blank">
                 <li className="inline text-[16px] hover:text-teal-700 text-base w-full cursor-pointer">{name}</li>
             </a>
         </div>
@@ -45,7 +43,7 @@ const Sidebar = () => {
                     if (!category.subcategory_list) {
                         if (!category.has_children) {
                             return (
-                                <Item name={category.name} image={category.image} link={category.topic_url} />
+                                <Item name={category.name} />
                             )
                         }
                     }
@@ -59,7 +57,7 @@ const Sidebar = () => {
                                 {!closedIds.includes(category.id) && (
                                     <ul className="space-y-4 text-gray-600 text-right mr-5">
                                         {category.subcategory_list.map((subcategory) => (
-                                            <Item name={subcategory.name} image={subcategory.image} link={category.topic_url} />
+                                            <Item name={subcategory.name} />
                                         ))}
                                     </ul>
                                 )}
@@ -68,30 +66,6 @@ const Sidebar = () => {
 
                     }
             })}
-            {/* <ul className="space-y-4 text-gray-600 text-right mr-5 mt-4 w-full">
-                {categories.slice(2, 6).map((category, index) => {
-                    return (
-                        <Item key={index} name={category.name} image={category.image} link={category.topic_url} />
-                    );
-                })}
-            </ul>
-            {categories.slice(6).map((category) => {
-                return (
-                    <>
-                        <div className="flex justify-between items-center w-48" onClick={() => toggle(category.id)} >
-                            <h3 className="font-semibold text-[16px] text-gray-800 text-right mb-4 mt-4 mr-4 cursor-pointer">{category.name}</h3>
-                            <img src={`/images/sidebar/arrow-down.png`} className={`w-6 h-6 ml-[-5px] cursor-pointer ${!closedIds.includes(category.id) ? 'rotate-180' : ''}`} />
-                        </div>
-                        {!closedIds.includes(category.id) && (
-                            <ul className="space-y-4 text-gray-600 text-right mr-5">
-                                {category.subcategory_list.map((subcategory) => (
-                                    <Item name={subcategory.name} image={subcategory.image} link={category.topic_url} />
-                                ))}
-                            </ul>
-                        )}
-                    </>
-                )
-            })} */}
         </div>
     );
 };
