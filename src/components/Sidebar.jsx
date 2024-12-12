f6ca711a2d41289ab3943cd68e0fe707c6a2a3be
Omeir Mujtaba <omeirr13@gmail.com>
2024-12-12 14:10:04 +0500

    fix

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -49,9 +49,11 @@ const Sidebar = ({ categories }) => {
             setClosedIds([...closedIds, id]);
         }
     }
+    console.log(categories);
     return (
         <div className="rounded-lg p-4 max-w-xs w-full h-screen mt-[2rem] hidden sm:block " dir="rtl" style={{ height: "calc(100vh - 73px)", width: "230px" }}>
-            {categories.forEach((category) => {
+            {categories.map((category) => {
+                console.log(category);
                 if (!category.subcategory_list) {
                     if (!category.has_children) {
                         return (
@@ -63,7 +65,7 @@ const Sidebar = ({ categories }) => {
                 }
                 else {
                     return (
-                        <React.Fragment key={category.id}>
+                        <div key={category.id}>
                             <div className="flex justify-between items-center w-48 mt-3" onClick={() => toggle(category.id)} >
                                 <h3 className="font-semibold text-[16px] text-gray-800 text-right mb-4 mt-4 mr-4 cursor-pointer">{category.name}</h3>
                                 <img src={`/images/sidebar/arrow-down.png`} alt="" className={`w-6 h-6 ml-[-5px] cursor-pointer ${!closedIds.includes(category.id) ? 'rotate-180' : ''}`} />
@@ -75,7 +77,7 @@ const Sidebar = ({ categories }) => {
                                     ))}
                                 </ul>
                             )}
-                        </React.Fragment>
+                        </div>
                     )
 
                 }
